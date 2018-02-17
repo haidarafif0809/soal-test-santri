72b9ed75ca154e4c45b10f4a4c8ea37ac46c99b4
galang <galangcahyapamungkas@gmail.com>
2018-02-17 13:17:03 +0700

    Ujian Galang

--- 001.php
+++ 001.php
@@ -10,58 +10,23 @@ contoh tingginya 4 maka hasilnya ketika di jalankan
 #####
 #######
  */
-echo "<br>";
-function jamku($tinggi)
+function jamPasir($angka)
 {
-    for ($i = 0; $i <= $tinggi; $i++) {
-        for ($j = 0; $j <= $i; $j++) {
-            echo "&nbsp;&nbsp";
+    $string  = '';
+    $string2 = '';
+    for ($i = $angka; $i >= 1; $i--) {
+        for ($string = 1; $string <= $i; $string++) {
+            echo $string;
         }
-        for ($k = $i; $k <= $tinggi - 1; $k++) {
-            echo "#";
-        }
-        for ($l = $i; $l < $tinggi - 1; $l++) {
-            echo "#";
-        }
-        echo "<br>";
+        // $string = $string . $i;
+        echo "\n";
     }
-    for ($i = 0; $i <= $tinggi; $i++) {
-        for ($j = $tinggi - 1; $j >= $i; $j--) {
-            echo "&nbsp;&nbsp;";
+    for ($i = 1; $i <= $angka; $i++) {
+        for ($string2 = 1; $string2 <= $i; $string2++) {
+            echo $string2;
         }
-        for ($k = 1; $k <= $i; $k++) {
-            echo "#";
-        }
-        for ($l = 1; $l < $i; $l++) {
-            echo "#";
-        }
-        echo "<br>";
+        // $string = $string . $i;
+        echo "\n";
     }
 }
-jamku(7);
-// function jam($tinggi)
-// {
-//     // base case
-//     // if ($tinggi == 0) {
-//     //     return;
-//     // }
-//     $count = strlen($tinggi);
-//     // echo $count;
-//     $u = '';
-//     for ($j = 1; $j <= $count; $j++) {
-//         $u .= $j;
-//         echo $u;
-//         echo "<br>";
-//     }
-//     // for ($i = $count; $i >= 1; $i--) {
-//     //     // $j = 1;
-//     //     // $j++;
-//     //     $hasil = substr($tinggi );
-//     //     // $hasil2 = substr($hasil);
-//     //     echo $hasil;
-//     //     // echo $hasil2[$j];
-//     //     // $j++;
-//     //     echo "<br>";
-//     // }
-// }
-// jam("12345");
+jamPasir(5);
