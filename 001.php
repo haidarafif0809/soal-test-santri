484304b846df884d690ce166b832bb0ef427f6a4
RindangRamadhan <rindangramadhan10@gmail.com>
2018-02-17 12:07:38 +0700

    Push Ujian

--- 001.php
+++ 001.php
@@ -2,12 +2,32 @@
 //Membuat Jam Pasir, ada sebuah function yang menerima sebuah angka, angka tersebut jadi patokan seberapa tinggi jam pasir nya.
 /*
 contoh tingginya 4 maka hasilnya ketika di jalankan
- #######
-  #####
-   ###
-    #
-   ###
-  #####
- #######
-*/
-?>
+#######
+#####
+###
+#
+###
+#####
+#######
+ */
+
+function jamPasir($angka)
+{
+    $hasil = "x";
+    for ($i = 0; $i < $angka; $i++) {
+        for ($a = $angka; $a > $i; $a--) {
+            echo $hasil;
+        }
+
+        echo "\n";
+
+    }
+    for ($i = ($angka - 2); $i >= 0; $i--) {
+        for ($a = $angka; $a > $i; $a--) {
+            echo $hasil;
+        }
+        echo "\n";
+    }
+}
+
+jamPasir(4);
--- 002.php
+++ 002.php
@@ -3,9 +3,16 @@
 Buat fungsi yang akan menerima panjang dan lebar, yang membuat jajar genjang seperti di bawah ini
 - - - - -
 \         \
-  \         \
-    \         \ 
-      \         \
-        - - - - -
-*/
-?>
+\         \
+\         \
+\         \
+- - - - -
+ */
+
+function jajarGenjang($panjang, $lebar)
+{
+    $hasil_panjang = "-";
+    $hasil_lebar   = "/";
+}
+
+jajarGenjang(4, 5);
