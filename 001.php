b96a9b06c012adcc1e5f3dab3e19159a96bf584c
Muhammad Khoirul Anwar <mkhoirulanwar41@gmail.com>
2018-02-17 13:06:45 +0700

    jawaban soal

--- 001.php
+++ 001.php
@@ -11,15 +11,6 @@ contoh tingginya 4 maka hasilnya ketika di jalankan
  #######
 */
 
-// function jamPasir($arg){
-// 	$string = '';
-// 	for($i = 1; $i <= $arg; $i++){
-// 		$string = $string.$i;
-// 		echo $string;
-// 		echo "\n";
-// 	}
-// }
-// jamPasir(4);
 function jamPasir($a){
 	for ($i = $a; $i > 0; $i--) {
 		for ($j = 1; $j <= $i; $j++) {
